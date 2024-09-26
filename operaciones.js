d686baf5169a2e4e4e255691507079a3203f3f64
Julius <porfin@MacBook-Pro-de-Julio-Olivares.local>
2024-09-26 00:40:42 -0300

    agregando operaciones

---
+++ operaciones.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+
+function registrar (nombre, edad, tipo, color, enfermedad ){
+  fs.readFile('./citas.json', 'utf-8', (error, data) => {
+    if (error) {
+      console.error('error al leer el archivo:', error)
+      return
+    }
+    // convertir el json en un arreglo
+    const citas = JSON.parse(data)
+
+    // crear un nuevo objeto con los datos del vehiculo
+    const nuevaCita = {
+      nombre,
+      edad,
+      tipo,
+      color,
+      enfermedad
+    }
+
+    citas.push(nuevaCita)
+
+    fs.writeFile('vehiculos.json', JSON.stringify(citas), (error) => {
+      if (error) {
+        console.error('error al registrar cita:', error)
+      } else {
+        console.log('cita registrada con exito')
+      }
+    })
+
+  });
+}
+
+const leer = () => {
+  fs.readFile('./citas.json', 'utf-8', (error, data)=> {
+    if (error) {
+      console.error('error al leer archivo:', error)
+      return;
+    }
+    try {
+      //se convierte el contenido del archivo JSON a un arreglo JS
+      const citas = JSON.parse(data)
+      //mostrando listado en la consola
+      console.log('listado vehiculos')
+      console.log(citas)
+    }
+    catch (parseError){
+      console.error('error al parsear el archivo JSON:', parseError)
+    }
+  })
+}
+
+
+module.exports = {registrar, leer}
